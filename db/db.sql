3ddd355dad82467f753bb84d3708eb21c3854a2a
Jenifer Jang <jeniferjangj@gmail.com>
2024-10-12 18:16:04 -0300

    Update db.sql

--- db/db.sql
+++ db/db.sql
@@ -10,4 +10,7 @@ CREATE TABLE People (
 );
 
 INSERT INTO People (name, age, cell_phone) VALUES
-('John', 30, '111-111-1111');
+('John', 30, '111-111-1111'),  
+('Beyonce', 33, '111111111111'),
+('Jay-Z', 57, '222222222222'),
+('Blue Ivy', 12, '333333333');
